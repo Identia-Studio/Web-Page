ea003488b3dcc812434251ef85bc27f93c7e8e6a
Yorch <j.andrade.dev@gmail.com>
2026-01-15 00:13:31 -0600

    redesigned header, hero and footer

--- components/layout/base_layout.tsx
+++ components/layout/base_layout.tsx
@@ -4,21 +4,16 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 import { CTASection } from "@/components/sections/home/cta-section"
 import { Navbar10 } from "@/components/ui/navbar-10";
 import { Footer } from "@/components/layout/footer"
-import { useRouter } from 'next/navigation';
 
 export default function BaseLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const router = useRouter()
   return (
     <ParallaxProvider>
       <div className="min-h-screen bg-black text-white">
         <Navbar10
-          onNavItemClick={(href) => {
-            router.push(href)
-          }}
         />
         <main>
           {children}
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,66 +1,119 @@
+import { MailIcon, MapPin, PhoneCallIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link"
 import { Parallax } from 'react-scroll-parallax';
+import ContactForm from "../sections/shared/contact-form";
 
 export function Footer() {
   return (
     <Parallax>
-      <footer className="container mx-auto max-w-screen-2xl py-12 px-8">
-        <div className="flex items-center flex-col md:flex-row space-y-8">
-          <div className="hidden space-y-2 md:block flex-1">
-            {/* <Link
-              href="tel:+529992755240"
-              className="block text-gray-400 hover:text-yellow-400 transition-colors font-text"
-            >
-              +52 9992 75 5240
-            </Link> */}
-            <Link
-              href="mailto:identiastudio25@gmail.com"
-              className="block text-gray-400 hover:text-yellow-400 transition-colors font-text"
-            >
-              identiastudio25@gmail.com
-            </Link>
-          </div>
-
-          <div className="flex-1 flex flex-col items-center">
+      <footer className="container mx-auto max-w-screen-2xl py-12 px-8" id="contact">
+        <div className="flex flex-col md:flex-row space-y-8">
+          <div className="w-full md:w-2/3 mb-12 md:mb-0">
             <Image
               src="/images/identia-studio-light.png"
               alt="Identia Studio: Light logo"
               width={200} 
               height={50} />
-            <p className="text-gray-400 text-sm font-text mt-4">
-              {new Date().getFullYear()}. Más que desarrollo: <strong>identidad digital.</strong>
-            </p>
+
+            
+            <div className="flex items-start gap-3 mt-8">
+              <MapPin className="text-yellow-400 w-[16px]" />
+              <div className="grid gap-2">
+                <p className="block text-yellow-400 transition-colors font-titles">
+                  Dirección
+                </p>
+                <Link
+                  href="tel:+529995446000"
+                  className="block text-white hover:text-yellow-400 transition-colors font-titles"
+                >
+                  Los Héroes, Yucatán, México. C.P. 97306.
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-3">
+              <PhoneCallIcon className="text-yellow-400 w-[16px]" />
+              <div className="grid gap-2">
+                <p className="block text-yellow-400 transition-colors font-titles">
+                  Oficina
+                </p>
+                <Link
+                  href="tel:+529995446000"
+                  className="block text-white hover:text-yellow-400 transition-colors font-titles"
+                >
+                  +52 999 544 6000
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-3">
+              <MailIcon className="text-yellow-400 w-[16px]" />
+              <div className="grid gap-2">
+                <p className="block text-yellow-400 transition-colors font-titles">
+                  Correo Electrónico
+                </p>
+                <Link
+                  href="mailto:identiastudio25@gmail.com"
+                  className="block text-white hover:text-yellow-400 transition-colors font-titles"
+                >
+                  identiastudio25@gmail.com
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4 mt-8">
+              <Link
+                href="https://www.linkedin.com/company/identia-studio-mid"
+                className="block border border-yellow-400 rounded-full p-2 text-yellow-400"
+                target="_blank"
+              >
+                <Image
+                  src='/images/linkedin.svg'
+                  width={16}
+                  height={16}
+                  alt="Identia Studio: LinkedIn @identia-studio-mid"
+                  className="fill-yellow-400 fill-current"
+                />
+              </Link>
+
+              <Link
+                href="https://instagram.com/identia.studio.mid"
+                className="block border border-yellow-400 rounded-full p-2"
+                target="_blank"
+              >
+                <Image
+                  src='/images/instagram.svg'
+                  width={16}
+                  height={16}
+                  alt="Identia Studio: Instagram @identia.studio.mid"
+                />
+              </Link>
+              <Link
+                href="https://www.facebook.com/identia.studio"
+                className="block border border-yellow-400 rounded-full p-2"
+                target="_blank"
+              >
+                <Image
+                  src='/images/facebook.svg'
+                  width={16}
+                  height={16}
+                  alt="Identia Studio: Facebook @identia.studio"
+                />
+              </Link>
+
+            </div>
           </div>
 
-          <div className="space-y-2 flex-1 text-center md:text-right">
-              {/* <Link
-                href="tel:+529992755240"
-                className="block text-gray-400 hover:text-yellow-400 transition-colors font-text md:hidden"
-              >
-                +52 9992 75 5240
-              </Link> */}
-              <Link
-                href="mailto:identiastudio25@gmail.com"
-                className="block text-gray-400 hover:text-yellow-400 transition-colors font-text md:hidden"
-              >
-                identiastudio25@gmail.com
-              </Link>
-            <Link
-              href="https://instagram.com/identia.studio.mid"
-              className="block text-gray-400 hover:text-yellow-400 transition-colors font-text"
-              target="_blank"
-            >
-              Instagram
-            </Link>
-            <Link
-              href="https://www.facebook.com/identia.studio"
-              className="block text-gray-400 hover:text-yellow-400 transition-colors font-text"
-              target="_blank"
-            >
-              Facebook
-            </Link>
+          <div className="w-full md:w-1/3">
+            <ContactForm />
           </div>
+
+        </div>
+        <div className="w-full">
+            <p className="text-gray-400 text-sm font-titles mt-4 text-center">
+            {new Date().getFullYear()} &copy; <strong>Identidad digital.</strong>
+          </p>
         </div>
       </footer>
     </Parallax>
