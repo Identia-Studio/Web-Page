71144906e3392909703e971e882a7c3b794d27e6
Yorch <j.andrade.dev@gmail.com>
2026-02-09 15:32:42 -0600

    fixed error

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -4,12 +4,12 @@ import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 // import { Testimonials } from "@/components/sections/home/testimonials"
 
-export const metadata = {
-  robots: {
-    index: false,
-    follow: false,
-  },
-};
+// export const metadata = {
+//   robots: {
+//     index: false,
+//     follow: false,
+//   },
+// };
 
 export default function Home() {
   const router = useRouter()
